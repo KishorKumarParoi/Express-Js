e5da4f58a07341dfee59ffa602aa88dc6023707a
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-10-15 20:47:18 +0600

    installed express

--- Raw/server.js
+++ Raw/server.js
@@ -1,16 +1,20 @@
 import http from 'http';
 
-http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('This is Home Page');
-        res.end();
-    } else if (req.url === '/about' && req.method === 'GET') {
-        res.write('This is about page');
-        res.end();
-    } else {
-        res.write('Page not found!');
-        res.end();
-    }
-}).listen(3000);
+const raw = http
+    .createServer((req, res) => {
+        if (req.url === '/') {
+            res.write('This is Home Page');
+            res.end();
+        } else if (req.url === '/about' && req.method === 'GET') {
+            res.write('This is about page');
+            res.end();
+        } else {
+            res.write('Page not found!');
+            res.end();
+        }
+    })
+    .listen(3000);
 
 console.log('Listening to port 3000');
+
+export default raw;
