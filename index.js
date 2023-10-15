11b5f31dbf792d826d54c51768b1e5152ad0887c
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-10-15 08:49:35 +0600

    changes two file and added main : index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import http from 'http';
+
+http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('This is Home Page');
+        res.end();
+    } else if (req.url === '/about' && req.method === 'GET') {
+        res.write('This is about page');
+        res.end();
+    } else {
+        res.write('Page not found!');
+        res.end();
+    }
+}).listen(3000);
+
+console.log('Listening to port 3000');
