4d716fc3466bfc3e9dc514d4b328ca66e481404f
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-10-15 21:03:23 +0600

    my first express.js code

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+ * Title :  Express Basics
+ * Description :  Learning Express.js
+ * Author : Kishor Paroi
+ * Date : 2023/10/15
+ * Time :  8:57:30 PM
+ */
+
+// dependencies
+import express from 'express';
+// module scaffolding
+const app = express();
+
+console.log(app);
+app.get('/', (req, res) => {
+    res.send('This is Home Page!');
+});
+
+app.post('/about', (req, res) => {
+    res.send('THis is about page');
+});
+
+app.listen(3000, () => {
+    console.log('listening to port 3000');
+});
