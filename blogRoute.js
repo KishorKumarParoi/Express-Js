ef2fa03281ca8e4628ce31f493684c1d7b6dc4a9
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-10-26 13:06:07 +0600

    changes for blogRouting mounting on'/blog'

--- blogRoute.js
+++ blogRoute.js
@@ -8,16 +8,15 @@
 
 import express from 'express';
 
-const app = express();
-const blog = express.Router();
+// const app = express();
+// const blog = express.Router();
 const blogAdmin = express.Router();
-const admin = express.Router();
+// const admin = express.Router();
 
-app.use('/blog', blog);
-blog.use('/admin', blogAdmin);
+// app.use('/blog', blog);
+// blog.use('/admin', blogAdmin);
 
-blogAdmin.use('/admin', admin);
-admin.use(express.json());
+// admin.use(express.json());
 blogAdmin.use(express.json());
 
 blogAdmin.get('/blogging', (req, res) => {
@@ -26,11 +25,11 @@ blogAdmin.get('/blogging', (req, res) => {
     res.send('blogging');
 });
 
-admin.get('/blogging', (req, res) => {
-    console.log(req.body);
-    console.log('blogging');
-    res.send('Admin blogging');
-});
+// admin.get('/blogging', (req, res) => {
+//     console.log(req.body);
+//     console.log('blogging');
+//     res.send('Admin blogging');
+// });
 
 // console.dir(app.path());
 // console.dir(blog.path());
