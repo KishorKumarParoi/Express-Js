dd1e499152b28a6d4ab7174d5ecfecd2c0aec014
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-10-29 18:49:34 +0600

    added multer package

---
+++ multer.js
@@ -0,0 +1,14 @@
+/*
+ * Title : Express File Upload Router
+ * Description : File Upload with Multer
+ * Author : Kishor Paroi
+ * Date : 2023/10/29
+ * Time : 6:47:23 PM
+ */
+
+import express from 'express';
+import multer from 'multer';
+
+const router = express.Router();
+console.log(multer);
+console.log(router);
