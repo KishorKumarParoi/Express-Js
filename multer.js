9eb901277b0c59cc92b18da8194dc0e45d2a4a3e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-10-30 10:01:42 +0600

    multer.js

--- multer.js
+++ multer.js
@@ -8,24 +8,37 @@
 
 import express from 'express';
 import multer from 'multer';
+import path from 'path';
 
 console.log(multer);
 console.log('Hello Kishor');
 
 // file upload folder
-const UPLOADS_FOLDER = './uploads/';
+// const UPLOADS_FOLDER = './uploads';
 
 // prepare the final multer upload object
-const upload = multer({
-    dest: UPLOADS_FOLDER,
-});
+// const upload = multer({
+//     dest: UPLOADS_FOLDER,
+// });
+
+// upload.firstName = 'Kishor';
+// upload.dest = UPLOADS_FOLDER;
+
+// console.log(upload);
 
 // module scaffolding
 const app = express();
+app.set('view engine', 'ejs');
+app.set('uploads', path.resolve('./uploads'));
+app.use(express.json());
 
 // application route
-app.post('/', upload.single('avatar'), (req, res) => {
-    res.send('Hello World!');
-});
+// app.post('/', upload.single('avatar'), (req, res) => {
+//     res.write('KKP');
+//     res.send('Hello World!');
+//     res.end();
+// });
+
+app.get('/', (req, res) => res.render('Homepage!'));
 
 export default app;
