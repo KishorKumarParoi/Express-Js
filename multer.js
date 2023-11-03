d100c910e3a9310649c9e6bdf0880146870729d4
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-03 09:41:06 +0600

    upload.fields added with two property

--- multer.js
+++ multer.js
@@ -31,12 +31,25 @@ app.use(express.json());
 // application route
 app.get('/', (req, res) => res.render('homepage'));
 
-app.post('/upload', upload.array('kkp', 3), (req, res) => {
-    console.log(req.body);
-    console.log(req.file);
-    res.send('Hello World!');
-    // res.redirect('/');
-});
+app.post(
+    '/upload',
+    upload.fields([
+        {
+            name: 'kkp',
+            maxCount: 1,
+        },
+        {
+            name: 'gallery',
+            maxCount: 3,
+        },
+    ]),
+    (req, res) => {
+        console.log(req.body);
+        console.log(req.file);
+        res.send('Hello World!');
+        // res.redirect('/');
+    }
+);
 
 app.listen(3000, () => {
     console.log('Listening to port 3000');
