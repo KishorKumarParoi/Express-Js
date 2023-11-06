ee96c9035baa89959972e1f49ce9499f0b66b8d9
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-06 11:29:34 +0600

    created app

--- mongoose.js
+++ mongoose.js
@@ -5,3 +5,16 @@
  * Date : 2023/11/06
  * Time : 11:25:28 AM
  */
+
+// dependencies
+import express from 'express';
+// module scaffolding
+const app = express();
+app.use(express.json());
+
+app.use('/', (req, res) => {
+    console.log(req.body);
+    res.send('Hello Kishor');
+});
+
+export default app;
