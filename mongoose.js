ace7e7d5796a440d756f87b3dc33704f316cbe85
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-06 19:33:09 +0600

    database connection with mongoose

--- mongoose.js
+++ mongoose.js
@@ -8,15 +8,34 @@
 
 // dependencies
 import express from 'express';
+import mongoose from 'mongoose';
+import punycode from 'punycode';
 
-// module scaffolding
+console.log(punycode);
+
+// express app initialization
 const app = express();
-
 app.use(express.json());
 
+// database connection with mongoose
+mongoose
+    .connect('mongodb://localhost/todos')
+    .then(() => console.log('Connection Succesfull'))
+    .catch((err) => console.log(err));
+
+// application routes
 app.use('/', (req, res) => {
     console.log(req.body);
     res.send('Hello Kishor');
 });
+
+// default error handlers
+function errorhandler(err, req, res, next) {
+    if (res.headerSent) {
+        return next(err);
+    }
+
+    return res.send(500).json({ error: err });
+}
 
 export default app;
