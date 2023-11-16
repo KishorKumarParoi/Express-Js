ce693df21f0f875d5be4d3101d80375b2a3fdac9
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-16 17:39:02 +0600

    created todohandler

---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,19 @@
+/*
+ * Title : To-Do Handler with Express.js
+ * Description : Creating a To-Do API with mongodb Database
+ * Author : Kishor Paroi
+ * Date : 2023/11/16
+ * Time : 5:35:12 PM
+ */
+
+// dependencies
+import express from 'express';
+import mongoose from 'mongoose';
+
+// express app initialization
+const app = express();
+app.use(express.json());
+console.log(mongoose);
+
+// export default app;
+export default app;
