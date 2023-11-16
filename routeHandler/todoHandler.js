4885f41f226fe42363722d47fcd5581c6c5ecc59
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-16 18:15:57 +0600

    created todo apis;

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -8,12 +8,39 @@
 
 // dependencies
 import express from 'express';
-import mongoose from 'mongoose';
 
 // express app initialization
-const app = express();
-app.use(express.json());
-console.log(mongoose);
+const router = express.Router();
 
-// export default app;
-export default app;
+// get all the todos
+router.get('/', async (req, res) => {
+    res.send('Hello Kishor');
+});
+
+// get a single todo with id
+router.get('/:id', async (req, res) => {
+    res.send('Hello Kishor');
+});
+
+// post a todo
+router.post('/', async (req, res) => {
+    res.send('Hello Kishor');
+});
+
+// post multiple todo
+router.post('/all', async (req, res) => {
+    res.send('Hello Kishor');
+});
+
+// put a todo
+router.put('/:id', async (req, res) => {
+    res.send('Hello Kishor');
+});
+
+// delete a todo
+router.delete('/:id', async (req, res) => {
+    res.send('Hello Kishor');
+});
+
+// export default router;
+export default router;
