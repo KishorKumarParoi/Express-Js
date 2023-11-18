6ee942de3cdb8c2159d594cc31004df64b36d3c8
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-18 08:27:15 +0600

    code refactored, added new 'Active API', removed redundancy

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -19,124 +19,132 @@ const router = express.Router();
 
 // get all the todos
 router.get('/', async (req, res) => {
-    // res.send('Hello Kishor');
-    await Todo.find({ status: 'inactive' })
-        .select({
-            _id: 0,
-            date: 0,
-        })
-        .limit(2)
-        .then((data) => {
-            res.status(200).json({
-                result: data,
-                message: 'Todo was fetched successfully!',
-            });
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: `${err}`,
-            });
+    try {
+        const data = await Todo.find({ status: 'inactive' })
+            .select({
+                _id: 0,
+                date: 0,
+            })
+            .limit(2);
+
+        res.status(200).json({
+            result: data,
+            message: 'Todos were fetched successfully!',
         });
+    } catch (err) {
+        res.status(500).json({
+            error: `${err}`,
+        });
+    }
+});
+
+// get active todo
+router.get('/active', async (req, res) => {
+    try {
+        const todo = new Todo();
+        const data = await todo.findActive();
+        res.status(200).json({
+            result: data,
+            message: 'Active Todos were fetched successfully!',
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: `${err}`,
+        });
+    }
 });
 
 // get a single todo with id
 router.get('/:id', async (req, res) => {
     // res.send('Hello Kishor');
-    await Todo.find({ _id: req.params.id })
-        .select({
-            _id: 0,
-            date: 0,
-        })
-        .limit(2)
-        .then((data) => {
-            res.status(200).json({
-                result: data,
-                message: 'Todo was fetched successfully!',
-            });
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: `${err}`,
-            });
+    try {
+        const data = await Todo.find({ _id: req.params.id })
+            .select({
+                _id: 0,
+                date: 0,
+            })
+            .limit(2);
+
+        res.status(200).json({
+            result: data,
+            message: 'Todo was fetched successfully!',
         });
+    } catch (err) {
+        res.status(500).json({
+            error: `${err}`,
+        });
+    }
 });
 
 // post a todo
 router.post('/', async (req, res) => {
-    // res.send('Hello Kishor');
-    console.log(req.body);
-    // const newToDo = new Todo(req.body);
-    await Todo.create(req.body)
-        // .save()
-        .then((data) => {
-            console.log(data);
-            res.status(200).json({
-                message: 'Todo was inserted successfully!',
-            });
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: `${err}`,
-            });
+    try {
+        const data = await Todo.create(req.body);
+        console.log(data);
+        res.status(200).json({
+            result: data,
+            message: 'Todo was inserted successfully!',
         });
+    } catch (err) {
+        res.status(500).json({
+            error: `${err}`,
+        });
+    }
 });
 
 // post multiple todo
 router.post('/all', async (req, res) => {
-    // res.send('Hello Kishor');
-    await Todo.insertMany(req.body)
-        .then((data) => {
-            console.log(data);
-            res.status(200).json({
-                message: 'Todo was inserted successfully!',
-            });
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: `${err}`,
-            });
+    try {
+        const data = await Todo.insertMany(req.body);
+        console.log(data);
+        res.status(200).json({
+            result: data,
+            message: 'Todo was inserted successfully!',
         });
+    } catch (err) {
+        res.status(500).json({
+            error: `${err}`,
+        });
+    }
 });
 
 // update a todo with given id
 router.put('/:id', async (req, res) => {
-    // res.send('Hello Kishor');
+    try {
+        const data = await Todo.findByIdAndUpdate(
+            { _id: req.params.id },
+            {
+                // $set: {
+                //     status: 'active',
+                //     title: 'Get a 500k Dollar Job',
+                //     author: 'Kishor Paroi',
+                // },
 
-    await Todo.findByIdAndUpdate(
-        { _id: req.params.id },
-        {
-            // $set: {
-            //     status: 'active',
-            //     title: 'Get a 500k Dollar Job',
-            //     author: 'Kishor Paroi',
-            // },
-
-            $set: req.body,
-        },
-        {
-            // returnOriginal: false,
-            // returnDocument: 'after',
-            new: false,
-            // useFindAndModify: false,
-        }
-    )
-        .then((data) => {
-            console.log(data);
-            res.status(200).json({
-                message: 'Todo was updated successfully!',
-            });
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: `${err}`,
-            });
+                $set: req.body,
+            },
+            {
+                // returnOriginal: false,
+                // returnDocument: 'after',
+                new: true,
+                // useFindAndModify: false,
+            }
+        );
+        console.log(data);
+        res.status(200).json({
+            result: data,
+            message: 'Todo was updated successfully!',
         });
+    } catch (err) {
+        res.status(500).json({
+            error: `${err}`,
+        });
+    }
 });
 
 // delete a todo
-router.delete('/:id', async (req, res) => {
-    // res.send('Hello Kishor');
-    await Todo.deleteOne({ _id: req.params.id })
+router.delete('/:id', (req, res) => {
+    // promise .then catch method
+    Todo.deleteOne({ _id: req.params.id })
         .then((data) => {
             res.status(200).json({
                 result: data,
