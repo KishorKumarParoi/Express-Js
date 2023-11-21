ca13643eed1677df0d1293e491e5735c4318aa62
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-11-21 17:57:31 +0600

    fixed Mongoose issue

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
     name: {
@@ -19,7 +19,7 @@ const userSchema = mongoose.Schema({
     },
     todos: [
         {
-            type: Mongoose.Types.ObjectId,
+            type: mongoose.Types.ObjectId,
             ref: 'Todo',
         },
     ],
